cd991bbb0fc617c4047d9608e838fdfdb20badf7
Arianna Basha <ariannagbasha@gmail.com>
2020-02-14 11:38:48 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+    // output the header row
+    document.write('<span class="cell header">&times;</span>');
+    for (let i = 0; i <= 10; i++) {
+        document.write('<span class="cell header">' + i + '</span>');
+    }
+    document.write('<br>');
+    // TODO: Write two nested for loops to output the rest of the multiplication table
