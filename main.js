0c7dac21ca7802f7b7bfff482cc95ebc40928131
Arianna Basha <ariannagbasha@gmail.com>
2020-02-18 18:14:51 -0600

    Needs so much fixing

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
-    // output the header row
-    document.write('<span class="cell header">&times;</span>');
-    for (let i = 0; i <= 10; i++) {
-        document.write('<span class="cell header">' + i + '</span>');
+// output the header row
+document.write('<span class="cell header">&times;</span>');
+for (let i = 0; i <= 10; i++) {
+    document.write('<span class="cell header">' + i + '</span>');
+}
+document.write('<br>');
+// TODO: Write two nested for loops to output the rest of the multiplication table
+for(let i = 0; i <= 10; i++) {
+    document.write('<span class="cell">' + i + '</span')
+    for(let j = 0; i <= 11; j++) {
+        document.write('<span class=cell' + i*j + '</span>')
+        document.write('<br>')
     }
-    document.write('<br>');
-    // TODO: Write two nested for loops to output the rest of the multiplication table
+}
