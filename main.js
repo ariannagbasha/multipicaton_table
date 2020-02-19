5dd2bd9100250da3382ff1202e4ca1d2ed99d8a4
Arianna Basha <ariannagbasha@gmail.com>
2020-02-19 10:03:38 -0600

    Fixed Multipication table

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ document.write('<span class="cell header">&times;</span>');
 for (let i = 0; i <= 10; i++) {
     document.write('<span class="cell header">' + i + '</span>');
     for(let j = 0; j <= 10; j++) {
-        document.write('span class="cell"' + i*j + '</span>')
+        document.write('<span class ="cell">' + i*j + '</span>');
     }
     document.write('<br>');
 }
